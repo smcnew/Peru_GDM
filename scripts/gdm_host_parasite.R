a89508eb5d0eb5b512d7ff0abea6604709df6982
Sabrina McNew <sabrinamcnew@dhcp-clara-7911.eduroam.cornell.edu>
2019-11-04 13:02:12 -0500

    Update GDM script

--- scripts/gdm_host_parasite.R
+++ scripts/gdm_host_parasite.R
@@ -17,7 +17,7 @@ library(dplyr) #we're a little tidy
 library(stringr) #used to rename categories for plots
 library(scales) #for alpha of colors
 library(vegan) #distance matrices
-
+set.seed(1987)
 
 # Read in data ------------------------------------------------------------
 community.number <- 1:18
@@ -63,10 +63,9 @@ peru_alt <- raster("./raw_data/peru_alt.grd") #read elevation raster
 npp <- raster("./formatted_data/npp.grd")
 
 
-
 # Functions ---------------------------------------------------------------
 # Create functions to run GDMs because we'll need to run many different models.
-# GDM takes basic form
+# Basic form of GDM:
 # turnover ~ rain + temp + elev + npp + parasite richness + symbiont turnover
 # where "symbiont" means predicting host turnover as function of parasite turnover
 # and vice versa.
@@ -101,7 +100,7 @@ p.gdm.fun <- function (r, e, d) {
                                  distPreds = d,
                                  predData = e.formatted, weightType="equal") #predictor data = scaled metadata
   dplyr::select(e.r.sitepair, -s1.matrix_2, -s2.matrix_2) -> e.r.sitepair #remove second matrix  gdm.varImp(e.r.sitepair, geo=T, parallel=T, nPerm = 100)
-  gdm.varImp(e.r.sitepair, geo=T, parallel=T, nPerm=1000)
+  gdm.varImp(e.r.sitepair, geo=T, parallel=T, nPerm=100)
 }
 
 # Environmental function, only takes climate/npp data, (no distance matrices)
@@ -114,3 +113,51 @@ gdm.fun.e <- function (r, e) {
                                  predData = e.formatted, weightType="equal") #predictor data = scaled metadata
   gdm(e.r.sitepair, geo=T)
 }
+
+# A function to select the best model from a permutated bunch,
+# based on most variance explained with fewest predictors
+modsel <- function(model){
+  best <- (model[[1]][2,] - sort(1:ncol(model[[1]]), decreasing=T)) %>% which.max #best model
+  na.omit(model[[2]][,best])
+}
+modsel(pb) %>% names()
+modsel(gdmgU) %>% round(digits=2) %>% scale() %>% rownames()
+
+
+
+
+# Parasite GDMs -----------------------------------------------------------
+
+# Distance matrices: parWunifracs (phylo turnover) parBC (species turnover)
+# Variables of interest: elevation, distance (lat/long), temp, precip, NPP,
+# host richness and host turnover
+# format input vars
+pe <- c("community.number", "community.Lat", "community.Long",
+        "tempPCA1", "precipPCA1","community.elev","total.host",
+        "npp.raster")
+pd <- list(hostJ, hostunifs)
+
+plot(gdm.fun(parBC, e=pe, d=pd), plot.layout = c(2,4)) #visualize full species model
+plot(gdm.fun(parGunifracs, e=pe, d=pd), plot.layout = c(2,4)) #visualize full phylo model
+
+# Permute model to test importance of predictor variables
+
+gdm_p_bc <- p.gdm.fun(parBC, pe, pd) #full model Bray-Curtis
+gdm_p_uf <- p.gdm.fun(parGunifracs, pe, pd) #full model generalized unifracs
+
+# Save models as R objects because they took a long ass time to run
+sapply(c("gdm_p_bc", "gdm_p_uf"), function (x)
+  saveRDS(get(x), file=paste("./GDM_results/", x, ".Rdata", sep="")))
+
+# Select best model
+modsel(gdm_p_bc) #Distance, Precip, Host turnover
+modsel(gdm_p_uf) #Distance, Precip, Host richness, Elevation
+
+
+# Best models; model select and then just run the GDM with the predictors we care about.
+gdm_b_par_spp <- gdm.fun(parBC, e= c("community.number", "community.Lat", "community.Long", "precipPCA1"), d=pd)
+
+# Host turnover not important for phylogenetic turnover so we use gdm.fun.e (no distance matrix predictor)
+gdm_b_par_phy <- gdm.fun.e(parGunifracs, e= c("community.number", "community.Lat", "community.Long", names(modsel(gdm_p_uf))[-1]))
+
+summary(gdm_b_par_phy)
